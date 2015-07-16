c3f223ec85d5bc3a84458d384bc7f583e2eb9fc0
dailey.yet <dailey.yet@gmail.com>
2015-07-16 17:55:05 +0800

    n

--- JavaFxDemo/src/demo/group/GroupMain.java
+++ JavaFxDemo/src/demo/group/GroupMain.java
@@ -1,7 +1,5 @@
 package demo.group;
 
-import java.util.Random;
-
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -16,14 +14,14 @@ public class GroupMain extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		 Group root = new Group();
-		    Scene scene = new Scene(root, 260, 80);
+		    Scene scene = new Scene(root, 440, 200);
 		    primaryStage.setScene(scene);
 		    primaryStage.setTitle("");
 
 		    VBox vb = new VBox();
 
 		    Pane canvas = new Pane();
-		    canvas.setStyle("-fx-background-color: black;");
+		    canvas.setStyle("-fx-background-color: white;");
 		    canvas.setPrefSize(200,200);
 		    int row_cell_count = 40;
 		    for(int i=0;i<500;i++){
@@ -36,26 +34,25 @@ public class GroupMain extends Application{
 			    canvas.getChildren().add(rectangle);
 		    }
 		    
-		    new Thread(){
-		    	public void run() {
-		    		for(int i=0;i<500;i++){
-				    	Random random = new Random();
-				    	int index=random.nextInt(500);
-				    	index=i;
-				    	Rectangle rectangle  = (Rectangle) canvas.getChildren().get(index);
-				    	rectangle.setFill(Color.GREEN);
-				    	try {
-							Thread.sleep(10);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-				    }
-		    	};
-		    }.start();
-		    
 		    vb.getChildren().add(canvas);
 		    scene.setRoot(vb);
+		    primaryStage.setOnShown((event)->{
+		    	 new Thread(){
+				    	public void run() {
+				    		for(int i=0;i<500;i++){
+						    	Rectangle rectangle  = (Rectangle) canvas.getChildren().get(i);
+						    	rectangle.setFill(Color.GREEN);
+						    	try {
+									Thread.sleep(10);
+								} catch (InterruptedException e) {
+									e.printStackTrace();
+								}
+						    }
+				    	};
+				    }.start();
+		    });
 		    primaryStage.show();
+		   
 	}
 	
 	public static void main(String[] args) {
